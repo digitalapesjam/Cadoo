306cde23d2b75ed24c555ad27d5f67d918e9cdd7
Paolo Burelli <paolo@h295.aau1x-2.klient.sydhavn.site.aau.dk>
2015-02-06 21:56:03 +0100

    forgot entities

---
+++ src/js/game/entities/logo.js
@@ -0,0 +1,18 @@
+var Logo = function Logo(game,posx,posy) {
+        this.game = game;
+        this.sprite = null;
+        this.anyCustomVariables = 5;
+        this.posx = posx;
+        this.posy = posy;
+}
+
+Logo.prototype.create= function() {
+        this.sprite = this.game.add.sprite(this.posx, this.posy, 'logo');
+        this.game.physics.arcade.enable(this.sprite);
+}
+
+Logo.prototype.update = function() {
+        this.game.physics.arcade.collide(this.sprite, Platform.group);
+}
+
+module.exports = Logo;
