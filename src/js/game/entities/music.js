35a21a02d043eeb4b0fce31af6401885240714ec
Francesco Zanitti <fzanitti@gmail.com>
2015-02-07 18:03:28 +0100

    cleaning

--- src/js/game/entities/music.js
+++ src/js/game/entities/music.js
@@ -1,14 +1,10 @@
 var Phaser = require('Phaser');
 
-var BgMusic = function(game, ent, worldHeight){ // steps, duration, easeFn) {
-  //this.duration = duration || 25000;
-  //this.steps = steps || 100;
+var BgMusic = function(game, ent, worldHeight){
   this.game = game;
   this.ent = ent;
   this.maxHeight = worldHeight * 7 / 8;
-//  this._intv = (this.duration / this.steps) / 1000;
-//  this._step = 0;
-//  this._t = 0;
+  this.killed = false;
 };
 
 BgMusic.prototype.create = function() {
@@ -16,26 +12,22 @@ BgMusic.prototype.create = function() {
   this.music = this.game.add.audio('music');
   var t = this;
   this.music.onDecoded.add(function(){
-    // t.music.volume = 0;
     t.music.play('', 0, 0, true);
-    // t.music.loop = true;
-//    t.game.add.tween(t.music).to({'volume': 1}, 25000, Phaser.Easing.Quintic.In, true, false);
   });
 };
 
 BgMusic.prototype.update = function(game) {
+  if (this.killed) {return;}
   if (this.player.falling) {
-    // if (this.music.loop === false) {
-    //   this.music.loop = true;
-    // }
     if (this.music.isPlaying && this.music.volume < 1) {
 
-      this.music.volume = Math.min(1, this.player.sprite.y / this.maxHeight);
-      //console.log(this.music.volume, this.player.y, this.maxHeight);
+      var x = Math.min(1, this.player.sprite.y / this.maxHeight);
+      this.music.volume = Phaser.Easing.Circular.In(x);
     }
   } else if (this.music.isPlaying) {
     console.log('kill audio');
     this.music.fadeOut();
+    this.killed = true;
   }
 };
 
