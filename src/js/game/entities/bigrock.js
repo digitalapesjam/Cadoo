df54e4cddbce8d88e0304caa965d42123dab9de9
jurgob <jurgo.boemo@gmail.com>
2015-02-07 03:23:37 +0100

    bigrock animation

--- src/js/game/entities/bigrock.js
+++ src/js/game/entities/bigrock.js
@@ -9,11 +9,16 @@ BigRock.prototype.create= function() {
         this.sprite = this.game.add.sprite(this.posx, this.posy, 'bigrock');
         this.game.physics.arcade.enable(this.sprite);
         this.sprite.body.allowGravity = true;
-        this.sprite.anchor.setTo(0.5, 1);
+        this.sprite.anchor.setTo(0.5, 0.5);
 }
 
 BigRock.prototype.update = function() {
         
+        var _delta = 1
+        if(this.sprite.rotation >= .2 || this.sprite.rotation <= -0.2  )
+            _delta = _delta * -1;
+        console.info(this.sprite.rotation)
+        this.sprite.rotation += (0.02 * _delta);
 }
 
 module.exports = BigRock;
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -6,6 +6,9 @@ module.exports = function(game) {
     game.load.image('logo', 'images/phaser.png#grunt-cache-bust');
     game.load.game.load.spritesheet ('stickman', 'images/stickman.png#grunt-cache-bust',32,32);
     game.load.image('bigrock', 'images/bigrock2.png#grunt-cache-bust');
+    game.load.script('filterBlurX', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/BlurX.js');
+    game.load.script('filterBlurY', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/BlurY.js');
+
   };
 
   preloader.create = function () {
