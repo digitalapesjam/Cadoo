e611186bd6567428159dcbba55233396f8624bd8
Paolo Burelli <burelli@gmail.com>
2015-02-07 22:23:59 +0100

    fixed tile repetitin

--- src/js/game/entities/sidetile.js
+++ src/js/game/entities/sidetile.js
@@ -8,7 +8,7 @@ var Sidetile = function Sidetile(game, sizex, sizey) {
 
 Sidetile.prototype.create = function () {
     this.sidetileLeft = this.game.add.tileSprite(0, 0, 80, this.sizey, 'leftsidetile');
-    this.sidetileRight = this.game.add.tileSprite(this.sizex - 80, -100, this.sizex, this.sizey + 100, 'rightsidetile');
+    this.sidetileRight = this.game.add.tileSprite(this.sizex - 80, -100, 80, this.sizey + 100, 'rightsidetile');
     
     
     this.game.physics.enable(this.sidetileLeft);
