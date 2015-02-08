702ba30c37dc32f6d0be1aa256844830e92e6c84
jurgob <jurgo.boemo@gmail.com>
2015-02-08 15:42:55 +0100

    new menu

--- src/js/game/states/menu.js
+++ src/js/game/states/menu.js
@@ -23,24 +23,15 @@ module.exports = function (game) {
          var tween = game.add.tween(textArea).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
          tween.start();
     }
-    function clearAllTimeouts(textArea){
-         
-         curTimeouts.forEach(function(timeoutIdx){
-                clearTimeout(timeoutIdx)
-            })
-    }
     
-    function showScreen(Obj1Text, Obj2Text, delay, hideAll){
+    
+    
+
+    function showScreen(Obj1Text, Obj2Text, delay){
             if(typeof(delay) !== 'number')
                 delay = 0
 
-
-            curShowTweens.forEach(function(tween){
-                tween.stop()
-            })
-            clearAllTimeouts();
-            hideAll();
-
+            
             Obj1Text.y =  game.world.centerY/2 - 100;
             
 
@@ -53,13 +44,18 @@ module.exports = function (game) {
                 Obj1Text.y = game.world.centerY/2;
 
         }
+    function centerIt(field){
+        field.x = (game.width/2) - field.width/2;  
+    }
 
     var gameState = {};
     gameState.create = function () {
-        var startGameText = ">Play";
-        var creditsText = "              >Credits";
+        var startGameText = "[ Play ]";
+        var creditsText = "Developed By:";
 
-        var style = { font: "32px Arial", fill: "#ffffff", align: "center" };
+        var styleTitle = { font: "34px Arial", fill: "#ffffff", align: "center"};
+        var style = { font: "32px Arial", fill: "#ffffff", align: "center"};
+        var styleSmall = { font: "26px Arial", fill: "#ffffff", align: "center"};
 
 
         var text1 = 'Sometimes you dream\n of falling...';
@@ -67,26 +63,29 @@ module.exports = function (game) {
 
         var text3 = 'Sometimes you dream\n of being in your bed...';
         var text4 = '...then you wake up...';
-        
-        var continueText = 'Play';
 
         var initYOffset = -5000;
-        var centeredTextX = game.world.centerX/2 - 20
+        var centeredTextX = game.width/2
 
+        var titleArea = game.add.text(centeredTextX, initYOffset, "CADOO", styleTitle);
+        centerIt(titleArea);
         var text1Area = game.add.text(centeredTextX, initYOffset, text1, style);
+        centerIt(text1Area);
         var text2Area = game.add.text(centeredTextX, initYOffset, text2, style);
-        var continueArea = game.add.text(centeredTextX, initYOffset, continueText, style);
-
+        centerIt(text2Area);
         var startGameButton = game.add.text(centeredTextX, initYOffset, startGameText, style);
-        startGameButton.anchor.set(0.5,0.5);
-        startGameButton.x = game.width/2;
-        
+        centerIt(startGameButton);
+
         var creditsButton = game.add.text(centeredTextX, initYOffset, creditsText, style);
+        centerIt(creditsButton);
 
         var text3Area = game.add.text(centeredTextX, initYOffset, text3, style);
+        centerIt(text3Area);
         var text4Area = game.add.text(centeredTextX, initYOffset, text4, style);
+        centerIt(text4Area);
 
-        var creditsArea = game.add.text(centeredTextX, initYOffset, 'Burelli Paolo \nBoemo Jurgo \nTurello Paolo \nZanitti Francesco', style);
+        var creditsArea = game.add.text(centeredTextX, initYOffset, 'Burelli Paolo (Web Designer) \nBoemo Jurgo (Geologist) \nTurello Paolo (Obstacle Makers) \nZanitti Francesco (Relationship Manager)', styleSmall);
+        centerIt(creditsArea);
         var returnInitArea = game.add.text(centeredTextX, initYOffset, '<Return To Intro', style);
 
         text1Area.inputEnabled = true;
@@ -104,37 +103,62 @@ module.exports = function (game) {
             text4Area.alpha = 0;
             startGameButton.alpha = 0;
             creditsButton.alpha = 0;
-            continueArea.alpha = 0;
             creditsArea.alpha = 0;
             returnInitArea.alpha = 0;
 
-            text1Area.y = 0;
-            text2Area.y = 0;
-            text3Area.y = 0;
-            text4Area.y = 0;
-            startGameButton.y = 0;
-            creditsButton.y = 0;
-            continueArea.y = 0;
-            creditsArea.y = 0;
-            returnInitArea.y = 0;
+            var hideOffesetY = -5000;
+
+            text1Area.y = hideOffesetY;
+            text2Area.y = hideOffesetY;
+            text3Area.y = hideOffesetY;
+            text4Area.y = hideOffesetY;
+            startGameButton.y = hideOffesetY;
+            creditsButton.y = hideOffesetY;
+            creditsArea.y = hideOffesetY;
+            returnInitArea.y = hideOffesetY;
         }
+        var hideCurScreen = function hideCurScreen(){
+            curShowTweens.forEach(function(tween){
+                tween.stop()
+            });
+            curTimeouts.forEach(function(timeoutIdx){
+                clearTimeout(timeoutIdx)
+            });
+            hideAll();
+        }
+
 
         var startGame = function(){
             game.state.start('preloader');
         }
         
-        var from2to3 = function(){
-            showScreen(text3Area, text4Area, 0, hideAll); 
+        var goIntro2 = function(){
+            hideCurScreen();
+            showScreen(text3Area, text4Area, 0); 
             curTimeouts.push(setTimeout(startGame,2500));
         } 
 
-        var from1to2 = function(){
-            showScreen(startGameButton, creditsButton, 0, hideAll); 
+        var goIntro1 = function(){
+            hideCurScreen();
+            showScreen(text1Area, text2Area, 0);
+            curTimeouts.push(setTimeout(goIntro2,4000));
         } 
 
         var showInitialScreen = function(){
-            showScreen(text1Area, text2Area, 0, hideAll);
-            curTimeouts.push(setTimeout(from1to2,4000));
+            hideCurScreen();
+
+            titleArea.y = 50
+            _showText(titleArea); 
+
+            startGameButton.y = 200;
+            _showText(startGameButton); 
+
+            creditsButton.y = 350;
+            _showText(creditsButton);
+
+            creditsArea.y = 400;
+            _showText(creditsArea);            
+
         }
 
         var showCredits = function(){
@@ -150,9 +174,12 @@ module.exports = function (game) {
 
         }
 
-        text1Area.events.onInputUp.add(from1to2);
-        text2Area.events.onInputUp.add(from1to2);
-        startGameButton.events.onInputUp.add(from2to3);
+        //startGameButton = game.add.button(game.world.centerX - 95, 400, 'button', from2to3, this, 2, 1, 0);
+        startGameButton.events.onInputUp.add(goIntro1);
+
+        text1Area.events.onInputUp.add(goIntro2);
+        text2Area.events.onInputUp.add(goIntro2);
+        
         text3Area.events.onInputUp.add(startGame);
         text4Area.events.onInputUp.add(startGame);
         
@@ -161,9 +188,7 @@ module.exports = function (game) {
 
         showInitialScreen();
         
-
         //game.add.text(game.world.centerX/2, 0, 'Quick Start', style)
-
         //game.add.text(centeredTextX, initYOffset, "Skip Intro", style)
 
     };
